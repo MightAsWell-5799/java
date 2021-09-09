10f1dfb263a17d86ac6bcd5e8315cd6a441fc2e2
MightAsWell-5799 <=>
2021-09-09 14:11:04 -0400

    first gui

--- classes/Store.java
+++ classes/Store.java
@@ -1,6 +1,6 @@
 package classes;
 
-public class Store {
+public class Store{
     // Integer ID and double sales private values
     /**
      * @param id    the integer id of the store
@@ -9,47 +9,61 @@ public class Store {
      */
     private int ID;
     private double sales;
+
     // constructor with integer ID parameter and a sales double parameter
     /**
      * 
      * @param ID
      * @param sales
-     * @param alias
      */
-    public Store(int ID, double sales) {
+    public Store(int ID, int sales) {
         this.ID = ID;
-        this.sales = sales;
+        this.sales = (double)sales/100;
     }
+
     /**
      * get the sales double of the store
+     * 
      * @return
      */
     public double getSales() {
         return sales;
     }
+
     /**
      * Set the sales double of the store
+     * 
      * @param sales
      */
     public void setSales(double sales) {
         this.sales = sales;
     }
 
+    public void setSales(int sales) {
+        this.sales = (double)sales/100;
+    }
+
     /**
      * the integer ID of the store
-     * @return  
+     * 
+     * @return
      */
     public int getID() {
         return ID;
     }
 
-    //toString override the default implementation that outputs "Store " + ID + ": " + sales 
-        /**
+    // toString override the default implementation that outputs "Store " + ID + ":
+    // " + sales
+    /**
      * 
      * @return
      */
     @Override
     public String toString() {
-        return String.format("Store "+ ID + " sold $%,.2f.", getSales());
+        return String.format("Store " + ID + " sold $%,.2f.", getSales());
+    }
+
+    public String toCSV() {
+        return getID() + "," + (int)(getSales()*100);
     }
 }
--- gameStop.java
+++ gameStop.java
@@ -1,22 +1,157 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridLayout;
 import classes.Store;
 
 public class gameStop {
     // hashmap to store the stores by integer ID
     private static HashMap<Integer, Store> stores = new HashMap<Integer, Store>();
+    private JFrame mainFrame = new JFrame();
 
     /**
      * execution args, not used
      * 
      * @param args
      */
-    public static void main(String[] args) {
+    gameStop() {
         readStores();
+        displayFirstOptions();
     }
+    public static void main(String[] args) {
+        new gameStop();
+    }
+
+    public void displayFirstOptions() {
+        mainFrame.dispose();
+        mainFrame = new JFrame();
+        mainFrame.setTitle("Game Stop");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton allStores = new JButton("Display All Stores");
+        JButton addStore = new JButton("Add A Store");
+        JButton removeStore = new JButton("Remove A Store");
+        JButton editStore = new JButton("Edit A Store");
+        JButton exit = new JButton("Exit");
+        JPanel panel = new JPanel();
+
+        //add listeners to each button
+        allStores.addActionListener((ActionListener)new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                panel.removeAll();
+                displayAllStores();
+            }
+        });
+        addStore.addActionListener((ActionListener) new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                panel.removeAll();
+                addStore();
+            }
+        });
+        removeStore.addActionListener((ActionListener)new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                panel.removeAll();
+                removeStore();
+            }
+        });
+        editStore.addActionListener((ActionListener)new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                panel.removeAll();
+                editStore();
+            }
+        });
+        
+        exit.addActionListener((ActionListener)new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        //add buttons to the frame
+        panel.add(allStores);
+        panel.add(addStore);
+        panel.add(removeStore);
+        panel.add(editStore);
+        panel.add(exit);
+        mainFrame.add(panel);
+        //set the frame properties
+        mainFrame.setSize(500, 500);
+        mainFrame.setLayout(new GridLayout(5, 6));
+        mainFrame.setVisible(true);
+    }
+
+    /**
+     * Displays all stores in the hashmap
+     */
+
+    public void displayAllStores() {
+        JPanel panel = new JPanel();
+        JButton back = new JButton("Back");
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                panel.removeAll();
+                displayFirstOptions();
+            }
+        });
+        mainFrame.setVisible(false);
+        panel.add(back);
+        mainFrame.add(panel);
+        mainFrame.setVisible(true);
+    }
+
+    public void addStore() {
+        JPanel panel = new JPanel();
+        JButton back = new JButton("Back");
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mainFrame.removeAll();
+                displayFirstOptions();
+            }
+        });
+        mainFrame.setVisible(false);
+        panel.removeAll();
+        panel.add(back);
+        mainFrame.add(panel);
+        mainFrame.setVisible(true);
+    }
+
+    public void removeStore() {
+        JPanel panel = new JPanel();
+        JButton back = new JButton("Back");
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mainFrame.removeAll();
+                displayFirstOptions();
+            }
+        });
+        mainFrame.setVisible(false);
+        panel.removeAll();
+        panel.add(back);
+        mainFrame.add(panel);
+        mainFrame.setVisible(true);
+    }
+
+    public void editStore() {
+        JPanel panel = new JPanel();
+        JButton back = new JButton("Back");
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mainFrame.removeAll();
+                displayFirstOptions();
+            }
+        });
+        mainFrame.setVisible(false);
+        panel.removeAll();
+        panel.add(back);
+        mainFrame.add(panel);
+        mainFrame.setVisible(true);
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
+
     // read in csv store info file with the first value of each row beinh the store
     // ID and the second being the store sales
     public static void readStores() {
@@ -33,7 +168,7 @@ public class gameStop {
                 storeID = Integer.parseInt(tempValues[0]);
                 sales = Integer.parseInt(tempValues[1]);
 
-                stores.put(storeID, new Store(storeID, (double) sales / 100));
+                stores.put(storeID, new Store(storeID, sales));
             }
             fis.close();
         } catch (IOException e) {
@@ -45,6 +180,43 @@ public class gameStop {
         }
     }
 
+    /**
+     * write stores map to csv file with key as the first value and the value as the
+     * second on every row
+     */
+    public static void writeStores() {
+        try {
+            File file = new File("stores.csv");
+            FileWriter fos = new FileWriter(file);
 
-    
+            for (Store store : stores.values()) {
+
+                System.out.println(store.toCSV());
+                fos.write(store.toCSV() + "\n");
+            }
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // add a store to the stores map
+    /**
+     * add a store to the stores map
+     * 
+     * @param store
+     */
+    public static void addStore(Store store) {
+        stores.put(store.getID(), store);
+    }
+
+    // remove a store from the stores map
+    /**
+     * remove a store from the stores map
+     * 
+     * @param storeID
+     */
+    public static void removeStore(int storeID) {
+        stores.remove(storeID);
+    }
 }
